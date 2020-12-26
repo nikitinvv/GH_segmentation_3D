71d29904f829c0b903382bbc7b447db8ff4c5525
Mikhail Fokin <58142687+mikhail-qwerty@users.noreply.github.com>
2020-12-26 14:29:07 +0700

    Add files via upload

---
+++ unet3D.py
@@ -0,0 +1,368 @@
+import os 
+import glob
+import random
+import numpy as np
+import skimage.io as io
+import tensorflow as tf
+import tifffile as tiff
+import matplotlib.pyplot as plt
+import skimage.transform as trans
+
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Dropout, Activation, concatenate
+from tensorflow.keras.layers import Conv2D, UpSampling2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import GlobalAveragePooling2D
+from tensorflow.keras.layers import Input, Concatenate
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow import Tensor
+
+from tensorflow.keras.layers import Conv3D, UpSampling3D
+from tensorflow.keras.layers import MaxPooling3D
+from tensorflow.keras.layers import GlobalAveragePooling3D
+from tensorflow.keras.layers import Conv3DTranspose
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+#                params
+#**********************************************
+
+# set visible GPU devices 
+os.environ["CUDA_VISIBLE_DEVICES"]= "0, 1"
+
+# set the minimal compute copability. 
+os.environ["TF_MIN_GPU_MULTIPROCESSOR_COUNT"]="6" 
+
+# set the maximum amount of memory in MB for one GPU
+memory_limit = 4000
+
+batch_size = 2
+
+# steps_per_epoch = number of images for training // batch_size
+steps_per_epoch = 378//batch_size
+
+#**********************************************
+
+'''
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_visible_devices(gpus[:], 'GPU')
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+'''
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        tf.config.experimental.set_visible_devices(gpus[:], 'GPU')
+        for gpu in gpus:
+            tf.config.experimental.set_virtual_device_configuration(gpu,
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=memory_limit)])
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)  
+
+
+# preparing data for training using generator 
+def dataGenerator(train_dir, image_dir, mask_dir, batch_size, target_size = (128, 128, 128)):  
+
+    i = 0 
+    im_list = glob.glob(train_dir + '/' + image_dir + '/' + '*.tif')
+    mask_list = glob.glob(train_dir + '/' + mask_dir + '/' + '*.tif')
+    while True:
+
+        image_batch = []
+        mask_batch = []
+
+        for b in range(batch_size):
+          
+            if i == len(im_list):
+                i = 0
+                data_list = list(zip(im_list, mask_list))
+                random.shuffle(data_list)
+                im_list, mask_list = zip(*data_list)
+
+            sample_im = im_list[i]
+            sample_mask = mask_list[i]
+            i += 1
+            im = tiff.imread(sample_im)/255
+            mask = tiff.imread(sample_mask)/255
+
+            im = trans.resize(im,target_size)
+            mask = trans.resize(mask,target_size)
+
+            im = np.reshape(im,im.shape + (1,))
+            mask = np.reshape(mask, mask.shape + (1,))
+            
+            image_batch.append(im)
+            mask_batch.append(mask)
+            
+        yield (np.array(image_batch), np.array(mask_batch))
+
+# define several deep learning models 
+
+def unet_2D(input_size = (128,128,1)):
+
+    inputs = tf.keras.Input(input_size)
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis = 3)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    model = Model(inputs = inputs, outputs = conv10)
+
+    return model
+
+
+
+def unet_3D(input_size = (128,128, 128, 1)):
+
+    inputs = tf.keras.Input(input_size)
+    conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conv1)
+
+    conv2 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling3D(pool_size=(2, 2, 2))(conv2)
+
+    conv3 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling3D(pool_size=(2, 2, 2))(conv3)
+
+    conv4 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling3D(pool_size=(2, 2, 2))(conv4)
+
+    conv5 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv3DTranspose(256, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv5), conv4], axis=4)
+    conv6 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv6), conv3], axis=4)
+    conv7 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv7), conv2], axis=4)
+    conv8 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(conv8), conv1], axis=4)
+    conv9 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv3D(1, (1, 1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    return model
+
+
+
+def dense_unet_3D(input_size = (128, 128, 128, 1)):
+
+    inputs = tf.keras.Input(input_size)
+    conv11 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(inputs)
+    conc11 = concatenate([inputs, conv11], axis=4)
+    conv12 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conc11)
+    conc12 = concatenate([inputs, conv12], axis=4)
+    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conc12)
+
+    conv21 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(pool1)
+    conc21 = concatenate([pool1, conv21], axis=4)
+    conv22 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conc21)
+    conc22 = concatenate([pool1, conv22], axis=4)
+    pool2 = MaxPooling3D(pool_size=(2, 2, 2))(conc22)
+
+    conv31 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(pool2)
+    conc31 = concatenate([pool2, conv31], axis=4)
+    conv32 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conc31)
+    conc32 = concatenate([pool2, conv32], axis=4)
+    pool3 = MaxPooling3D(pool_size=(2, 2, 2))(conc32)
+
+    conv41 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(pool3)
+    conc41 = concatenate([pool3, conv41], axis=4)
+    conv42 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conc41)
+    conc42 = concatenate([pool3, conv42], axis=4)
+    pool4 = MaxPooling3D(pool_size=(2, 2, 2))(conc42)
+
+    conv51 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(pool4)
+    conc51 = concatenate([pool4, conv51], axis=4)
+    conv52 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(conc51)
+    conc52 = concatenate([pool4, conv52], axis=4)
+
+    up6 = concatenate([Conv3DTranspose(256, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc52), conc42], axis=4)
+    conv61 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(up6)
+    conc61 = concatenate([up6, conv61], axis=4)
+    conv62 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conc61)
+    conc62 = concatenate([up6, conv62], axis=4)
+
+    up7 = concatenate([Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc62), conv32], axis=4)
+    conv71 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(up7)
+    conc71 = concatenate([up7, conv71], axis=4)
+    conv72 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conc71)
+    conc72 = concatenate([up7, conv72], axis=4)
+
+    up8 = concatenate([Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc72), conv22], axis=4)
+    conv81 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(up8)
+    conc81 = concatenate([up8, conv81], axis=4)
+    conv82 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conc81)
+    conc82 = concatenate([up8, conv82], axis=4)
+
+    up9 = concatenate([Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc82), conv12], axis=4)
+    conv91 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(up9)
+    conc91 = concatenate([up9, conv91], axis=4)
+    conv92 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conc91)
+    conc92 = concatenate([up9, conv92], axis=4)
+
+    conv10 = Conv3D(1, (1, 1, 1), activation='sigmoid')(conc92)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    return model
+
+
+
+def res_unet_3D(input_size = (128, 128, 128, 1)):
+
+    inputs = tf.keras.Input(input_size)
+    conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conv1)
+    conc1 = concatenate([inputs, conv1], axis=4)
+    pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conc1)
+
+    conv2 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conv2)
+    conc2 = concatenate([pool1, conv2], axis=4)
+    pool2 = MaxPooling3D(pool_size=(2, 2, 2))(conc2)
+
+    conv3 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv3)
+    conc3 = concatenate([pool2, conv3], axis=4)
+    pool3 = MaxPooling3D(pool_size=(2, 2, 2))(conc3)
+
+    conv4 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conv4)
+    conc4 = concatenate([pool3, conv4], axis=4)
+    pool4 = MaxPooling3D(pool_size=(2, 2, 2))(conc4)
+
+    conv5 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(conv5)
+    conc5 = concatenate([pool4, conv5], axis=4)
+
+    up6 = concatenate([Conv3DTranspose(256, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc5), conv4], axis=4)
+    conv6 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(conv6)
+    conc6 = concatenate([up6, conv6], axis=4)
+
+    up7 = concatenate([Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc6), conv3], axis=4)
+    conv7 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(conv7)
+    conc7 = concatenate([up7, conv7], axis=4)
+
+    up8 = concatenate([Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc7), conv2], axis=4)
+    conv8 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(conv8)
+    conc8 = concatenate([up8, conv8], axis=4)
+
+    up9 = concatenate([Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(conc8), conv1], axis=4)
+    conv9 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(conv9)
+    conc9 = concatenate([up9, conv9], axis=4)
+
+    conv10 = Conv3D(1, (1, 1, 1), activation='sigmoid')(conc9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    return model
+
+
+# train model using Tensorflow distribute stratagy
+
+strategy = tf.distribute.MirroredStrategy()
+
+with strategy.scope():
+    model = dense_unet_3D(input_size=(64, 64, 64,1))
+
+
+myGene = dataGenerator(train_dir = 'train/256', image_dir = 'images', mask_dir = 'labels', batch_size = batch_size, target_size = (64, 64, 64))
+
+model.summary()
+
+model_checkpoint = ModelCheckpoint('unet_3D.hdf5', monitor='loss',verbose=1, save_best_only=True)
+
+model.compile(optimizer=Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.000000199), loss='binary_crossentropy', metrics=['accuracy'])
+
+history = model.fit(myGene,steps_per_epoch=steps_per_epoch,epochs=50,callbacks=[model_checkpoint])
+
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train'], loc='upper left')
+plt.show()
